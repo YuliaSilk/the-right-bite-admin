76f6a80378efcd30ac661378a9e9006f5e7f3353
YuliaSilk <yuliashevchyk0506@gmail.com>
2025-11-22 12:05:22 +0200

    fixed build

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,30 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
  plugins: [react(), tailwindcss()],
-
+ build: {
+  chunkSizeWarningLimit: 1000,
+  rollupOptions: {
+   output: {
+    manualChunks(id) {
+     if (id.includes("node_modules/antd")) {
+      return "antd";
+     }
+     if (id.includes("@ant-design/icons")) {
+      return "antd-icons";
+     }
+     if (id.includes("node_modules/react") || id.includes("node_modules/react-dom")) {
+      return "react-vendor";
+     }
+     if (id.includes("node_modules/react-router-dom")) {
+      return "router";
+     }
+     if (id.includes("node_modules")) {
+      return "vendor";
+     }
+    },
+   },
+  },
+ },
  server: {
   port: 8080,
   host: "0.0.0.0",
